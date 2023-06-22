10e930bffab07464f79a86971c438e864b0af128
Snickers333 <bkkami2@gmail.com>
2023-06-22 15:44:19 +0200

    Finished Project

---
+++ Excercise1/Excercise1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+if (args.Length < 1) throw new ArgumentNullException();
+
+var url = args[0];
+if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+{
+    throw new ArgumentException();
+}
+
+var httpClient = new HttpClient();
+HttpResponseMessage response = await httpClient.GetAsync(url);
+if (!response.IsSuccessStatusCode)
+{
+    throw new Exception("Błąd w czasie pobierania strony");
+}
+
+string content = await response.Content.ReadAsStringAsync();
+var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+MatchCollection matches = regex.Matches(content);
+
+if (matches.Count < 1)
+{
+    throw new Exception("Nie znaleziono adresów email");
+}
+
+var distinctMatches = new HashSet<string>();
+foreach (Match match in matches)
+{
+    distinctMatches.Add(match.ToString());
+}
+
+foreach (string s in distinctMatches)
+{
+    Console.WriteLine(s);
+}
